7dcf11bb60864ef7778c24286a8f111355c0f5bd
Vitor Lucas <vitorcrispim@yahoo.com.br>
2024-01-02 18:20:50 -0300

    Adicionando a documentação e o readme

--- src/main/java/com/api/vitorlucascrispim/files/controllers/FileStorageController.java
+++ src/main/java/com/api/vitorlucascrispim/files/controllers/FileStorageController.java
@@ -2,6 +2,8 @@ package com.api.vitorlucascrispim.files.controllers;
 
 
 import com.api.vitorlucascrispim.files.services.FileStorageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -21,8 +23,21 @@ public class FileStorageController {
     @Autowired
     private FileStorageService fileStorageService;
 
-    
+
+    private static final String ENDPOINT_NAME = "FilesAPI - Gerenciamento de arquivos";
+    private static final String UPLOAD_RESUMO = "Esse endpoint permite que um arquivo seja enviado para que posteriormente seja feito o download";
+    private static final String UPLOAD_DESCRICAO = "Esse endpoint permite o envio de um arquivo utilizando uma requisição do tipo multipart/form-data informando caso sucesso o link de download do arquivo ";
+
+    private static final String DOWNLOAD_RESUMO = "Esse endpoint permite o download do arquivo caso ele esteja disponivel, utilizando o nome juntamente com tipo de arquivo";
+    private static final String DOWNLOAD_DESCRICAO = "Esse endpoint fornece um arquivo caso o mesmo tenha sido enviado posteriormente e faz o download do mesmo  ";
+
+
+    private static final String LIST_ARCHIVE_RESUMO = "Esse endpoint lista os arquivos previamente enviados";
+    private static final String LIST_ARCHIVE_DESCRICAO = "Esse endpoint retorna uma lista com todos os arquivos juntamente com seus tipos para realizar o download  ";
+
     @PostMapping(path = "/upload",consumes = {"multipart/form-data"})
+    @Tag(name = ENDPOINT_NAME )
+    @Operation(summary = UPLOAD_RESUMO,description = UPLOAD_DESCRICAO)
     public ResponseEntity<String> uploadFile(@RequestParam(value = "file",required = true)MultipartFile file){
         try{
             String uploadUrl = fileStorageService.upload(file);
@@ -33,12 +48,16 @@ public class FileStorageController {
     }
 
     @GetMapping("/download/{fileName:.+}")
+    @Tag(name = ENDPOINT_NAME )
+    @Operation(summary = DOWNLOAD_RESUMO,description = DOWNLOAD_DESCRICAO)
     public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request){
         ResponseEntity download = fileStorageService.download(fileName, request);
         return download;
     }
 
     @GetMapping("/archives")
+    @Tag(name = ENDPOINT_NAME )
+    @Operation(summary = LIST_ARCHIVE_RESUMO,description = LIST_ARCHIVE_DESCRICAO)
     public  ResponseEntity<List<String>> allArchives(){
             List<String> archivesList = fileStorageService.archivesList();
             return ResponseEntity.ok(archivesList);
