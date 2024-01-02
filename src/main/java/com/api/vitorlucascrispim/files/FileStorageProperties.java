7d71560ce621981e2b97835416e8933f693e80a6
Vitor Lucas <vitorcrispim@yahoo.com.br>
2024-01-02 14:30:17 -0300

    Finalizando a api de arquivos

---
+++ src/main/java/com/api/vitorlucascrispim/files/FileStorageProperties.java
@@ -0,0 +1,22 @@
+package com.api.vitorlucascrispim.files;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "file")
+public class FileStorageProperties {
+
+
+    @Value("${file.upload.directory}")
+    private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+}
